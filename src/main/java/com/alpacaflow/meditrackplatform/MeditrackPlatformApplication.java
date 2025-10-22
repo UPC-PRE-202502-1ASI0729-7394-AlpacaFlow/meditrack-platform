4c5a9e39e0c3e8ff0627f6d54e0dbf6375b2c2ef
Gabriel Rivera <gabriverlay@gmail.com>
2025-10-21 20:53:24 -0500

    chore: first commit, created the spring boot application

---
+++ src/main/java/com/alpacaflow/meditrackplatform/MeditrackPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.alpacaflow.meditrackplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditrackPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeditrackPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpacaflow/meditrackplatform/MeditrackPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpacaflow.meditrackplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
